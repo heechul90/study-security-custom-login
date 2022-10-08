15a1bcc6246f77aacbef1fc2d5909e595b218153
hclee <heechul4296@gmail.com>
2022-10-09 01:11:23 +0900

    spring security > SecurityFilterChain 추가

--- src/main/java/study/spring/security/config/SecurityConfiguration.java
+++ src/main/java/study/spring/security/config/SecurityConfiguration.java
@@ -12,8 +12,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @Configuration
 @EnableWebSecurity(debug = true)
 @EnableGlobalMethodSecurity(prePostEnabled = true)
