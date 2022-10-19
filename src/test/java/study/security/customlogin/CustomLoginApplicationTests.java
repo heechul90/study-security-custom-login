60d0f6eb97e6f46b98b98899b83903495f23701d
hclee <hclee@dataeum.kr>
2022-10-19 13:21:00 +0900

    spring security custom login source

---
+++ src/test/java/study/security/customlogin/CustomLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package study.security.customlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
